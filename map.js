f20066fbadc53ea9624325f3dc072ab31b70fc5e
Josuah Demangeon <josuah.demangeon>
2015-07-05 23:11:31 +0200

    Plain html website instead of jekyll-based

---
+++ map.js
@@ -0,0 +1,38 @@
+//*
+
+//*/
+function initialize() {
+    var mapCanvas = document.getElementById('map-canvas');
+    var mapOptions = {
+        center: new google.maps.LatLng(48.096918, -1.978071),
+        zoom: 10,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+	disableDefaultUI: true
+    }
+    var map = new google.maps.Map(mapCanvas, mapOptions)
+    map.set('styles', [
+	{
+	    featureType: 'road',
+	    elementType: 'geometry.stroke',
+	    stylers: [
+		{ color: '#ff0066' },
+		{ weight: 0.3 }
+	    ]
+	}, {
+	    featureType: 'landscape',
+	    elementType: 'geometry',
+	    stylers: [
+		{ color: '#eeeeee' }
+	    ]
+	}
+    ]);
+    var marker = new google.maps.Marker({
+	position: new google.maps.LatLng(48.036833, -1.819001),
+	map: map,
+	icon: 'img/map_icon.png',
+	animation: google.maps.Animation.DROP
+});
+}
+
+
+google.maps.event.addDomListener(window, 'load', initialize);
